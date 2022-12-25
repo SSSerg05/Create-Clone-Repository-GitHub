87754eb8f95fca5b368b3d1e3cea450ebe1697bb
SSSerg05 <ssserg05@gmail.com>
2022-12-26 01:40:36 +0200

    edit script for different selected
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+// const refs = {
+//     openModalBtn: document.querySelector('[data-open-modal]'),
+//     closeModalBtn: document.querySelector('[data-close-modal]'),
+//     backdrop: document.querySelector('[data-backdrop]'),
+// };
+
+// refs.openModalBtn.addEventListener("click", toggleModal);
+// refs.closeModalBtn.addEventListener("click", toggleModal);
+
+// refs.backdrop.addEventListener("click", logBackdropClick);
+
+// function toggleModal() {
+//     refs.backdrop.classList.toggle('is-hidden');
+// }
+
+// function logBackdropClick() {
+//     console.log('Це клік у бекдроп');
+// }
+
 const refs = {
-    openModalBtn: document.querySelector('[data-open-modal]'),
-    closeModalBtn: document.querySelector('[data-close-modal]'),
-    backdrop: document.querySelector('[data-backdrop]'),
+    openModalBtns: document.querySelectorAll('[data-open-modal]'),
+    closeModalBtns: document.querySelectorAll('[data-close-modal]'),
+    backdrop: document.querySelectorAll('[data-backdrop]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+for(let item of refs.openModalBtns) {
+    item.addEventListener("click", toggleModal(item));
+    console.log(item);
+}
+console.log(refs.openModalBtns);
 
-refs.backdrop.addEventListener("click", logBackdropClick);
 
-function toggleModal() {
-    refs.backdrop.classList.toggle('is-hidden');
+for(let item of refs.closeModalBtns) {
+   item.addEventListener("click", toggleModal(item));
+}
+
+for(let item of refs.backdrop) {
+    item.addEventListener("click", logBackdropClick);
+}
+
+function toggleModal(item) {
+    refs.backdrop[item].classList.toggle('is-hidden');
 }
 
 function logBackdropClick() {
